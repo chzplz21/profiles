1e6c3dec3a3ca62e47c54e0895df99dea015114a
David <drothhello@gmail.com>
2019-03-01 12:41:55 -0500

    changedNames

--- resources/views/frontpage.blade.php
+++ resources/views/frontpage.blade.php
@@ -3,7 +3,7 @@
 @section('content')
    
     @include('search.searchBox')
-        <h2 class = "recentH2">Most Recent Profiles</h2>
+        <h2 class = "recentH2">Featured Profiles</h2>
         @foreach ($recentPosts as $user)
             @include('littleUserBox')
             
